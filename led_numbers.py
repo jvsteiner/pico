5f17e6c594d3502291769035f836cc60cdceec9d
Jamie Steiner <jvsteiner@gmail.com>
2024-01-17 12:38:09 +0200

    added scrolling function

--- led_numbers.py
+++ led_numbers.py
@@ -25,6 +25,46 @@ num = [
     0x8E,
 ]
 
+CHARS = {
+    "0": 0xC0,
+    "1": 0xF9,
+    "2": 0xA4,
+    "3": 0xB0,
+    "4": 0x99,
+    "5": 0x92,
+    "6": 0x82,
+    "7": 0xF8,
+    "8": 0x80,
+    "9": 0x90,
+    "A": 0x88,
+    "B": 0x83,
+    "C": 0xC6,
+    "D": 0xA1,
+    "E": 0x86,
+    "F": 0x8E,
+    "G": 0x82,
+    "H": 0b10001001,
+    "I": 0b11111011,
+    "J": 0b11100001,
+    "K": 0b10000111,
+    "L": 0b11000111,
+    "N": 0b10101011,
+    "O": 0b10100011,
+    "P": 0b10001100,
+    "R": 0b10101111,
+    "S": 0b10010010,
+    "T": 0b11001110,
+    "U": 0b11100011,
+    "V": 0b11100011,
+    "Y": 0x99,
+    "Z": 0x92,
+    " ": 0b11111111,
+    "-": 0b10111111,
+    "_": 0b11110111,
+}
+
+VALID_CHARS = list(CHARS.keys())
+
 
 def led_display():
     for i in range(0, 4):
@@ -42,12 +82,50 @@ def led_display():
 chip = Chip74HC595(18, 20, 21)
 
 
-def display(digit, number):
+def display_raw(digit, bin):
     chns = Pin(comPin[digit], Pin.OUT)
-    chip.shiftOut(0, num[number])
+    chip.shiftOut(0, bin)
     chns.value(1)
     time.sleep_ms(1)
     chns.value(0)
+
+
+def display(digit, number):
+    display_raw(digit, num[number])
+
+
+def display_char(digit, char):
+    display_raw(digit, CHARS[char])
+
+
+def display_four_chars(chars):
+    for i in [3, 2, 1, 0]:
+        display_char(i, chars[i])
+    time.sleep_ms(1)
+
+
+def display_four_raw(chars):
+    for i in [3, 2, 1, 0]:
+        display_raw(i, chars[i])
+    time.sleep_ms(1)
+
+
+def add_dots(chars):
+    output = []
+    for char in chars:
+        if char in VALID_CHARS:
+            output.append(CHARS[char])
+        if char == ".":
+            output[-1] = output[-1] - 128
+    return output
+
+
+def scroll_chars(chars):
+    chars = "    " + chars + "    "
+    chars = add_dots(chars)
+    for i in range(0, len(chars) - 3):
+        display_task(display_four_raw, chars[i : i + 4], duration=0.5)
+    time.sleep_ms(1)
 
 
 def display_four_digits(number):
@@ -79,15 +157,9 @@ def countdown(hi=10, duration=1):
 # asyncio.create_task(clock_task())
 # asyncio.run(clock_task())
 # asyncio.wait_for(clock_task, 5)
+# display_task(display_four_chars, "ATLF")
 
 
 def localtime(secs=None, offset=TZ_OFFSET):
     """Convert the time secs expressed in seconds since the Epoch into an 8-tuple which contains: (year, month, mday, hour, minute, second, weekday, yearday) If secs is not provided or None, then the current time from the RTC is used."""
     return time.localtime((secs if secs else time.time()) + offset)
-
-
-try:
-    while True:
-        led_display()
-except:
-    pass
